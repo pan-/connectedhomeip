ca67f4bc2b960ceff70a11da895a506855594b4f
Vincent Coubard <vincent.coubard@arm.com>
2021-06-09 10:01:12 +0100

    Hide Github token in shell script running on lava

--- scripts/tests/lava/run_chip_unit_tests.sh
+++ scripts/tests/lava/run_chip_unit_tests.sh
@@ -2,10 +2,11 @@
 
 set -xe
 
-# Fix me: Check arguments validity
+set +x
 GITHUB_REPOSITORY=$1
 GITHUB_ARTIFACT_NAME=$2
 GITHUB_TOKEN=$3
+set -x
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")" # Get the directory name
 SCRIPT_DIR="$(realpath "${SCRIPT_DIR}")"    # Resolve its full path if need be
@@ -13,7 +14,9 @@ SCRIPT_DIR="$(realpath "${SCRIPT_DIR}")"    # Resolve its full path if need be
 source $SCRIPT_DIR/common.sh
 
 echo "Downloading artifact: $GITHUB_ARTIFACT_NAME in $GITHUB_REPOSITORY"
+set +x
 download_artifacts $GITHUB_REPOSITORY $GITHUB_ARTIFACT_NAME $GITHUB_TOKEN archive.zip
+set -x
 
 unzip archive.zip
 
