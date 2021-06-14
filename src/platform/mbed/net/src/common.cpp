972edd6939f9d9265046b31428a65d98391f3bf8
Vincent Coubard <vincent.coubard@arm.com>
2021-06-14 23:11:35 +0100

    Fix Mbed to POSIX address conversion

--- src/platform/mbed/net/src/common.cpp
+++ src/platform/mbed/net/src/common.cpp
@@ -32,18 +32,18 @@ int convert_mbed_addr_to_bsd(struct sockaddr * out, const SocketAddress * in)
         return -1;
     }
 
-    nsapi_version_t verison = in->get_ip_version();
-    if (verison == NSAPI_IPv4)
+    nsapi_version_t version = in->get_ip_version();
+    if (version == NSAPI_IPv4)
     {
         sockaddr_in * addr = reinterpret_cast<sockaddr_in *>(out);
         addr->sin_family   = AF_INET;
         addr->sin_port     = htons(in->get_port());
         memcpy(&addr->sin_addr, in->get_ip_bytes(), NSAPI_IPv4_BYTES);
     }
-    else if (verison == NSAPI_IPv6)
+    else if (version == NSAPI_IPv6)
     {
         sockaddr_in6 * addr = reinterpret_cast<sockaddr_in6 *>(out);
-        addr->sin6_family   = AF_INET;
+        addr->sin6_family   = AF_INET6;
         addr->sin6_port     = htons(in->get_port());
         memcpy(&addr->sin6_addr, in->get_ip_bytes(), NSAPI_IPv6_BYTES);
     }
